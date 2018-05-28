476d2f44b7c75544fb7d9ddbf6b0bc17ac97c359
Carbo <Carbo@MRCARBOSH>
2018-05-28 23:42:01 +0300

    Добавлений ресурс музики

--- GiT123/GiT123.cpp
+++ GiT123/GiT123.cpp
@@ -3,35 +3,40 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <mmsystem.h>
-
+#include <windows.h>
+#include <Mmsystem.h>
+#include "resource.h"
+#define PlaySound 
 using namespace std;
-#pragma comment(lib, "winmm.lib")
-#define PlaySound;
 int main()
 {
-	cout << "1 Ламнов Олександр Олександрович";
-	cout << "2 11:02:1997";
-	cout << "3 Піщанка";
-	cout << "4 Мама";
-	cout << "5 Папа";
-	cout << "6 2003р Першого Вересня";
-	cout << "7 2004р. Першого Вересня";
-	cout << "8 Скрипка";
-	cout << "9 Піаніно";
-	cout << "10 2012р. закінчення навчання в муз школі.";
-	cout << "11 9 клас ";
-	cout << "12 2014 рік";
-	cout << "13 Одеский коледж комп'ютерних техгологій 2014р";
-	cout << "14 Захистив диплом на 5 балів";
-	cout << "15 2017р";
-	cout << "16 Курси булгалтерсього обліку";
-	cout << "17 Донну 2017";
-	cout << "18 Музика";
-	cout << "19 3-д моделювання";
-	cout << "20 Спорт";
-	PlaySound("Sound.wav", NULL, SND_FILENAME | SND_ASYNC);
+	setlocale(LC_ALL, "Russian");
+	cout << "1 Ламнов Олександр Олександрович\n";
+	cout << "2 11:02:1997\n";
+	cout << "3 Піщанка\n";
+	cout << "4 Мама\n";
+	cout << "5 Папа\n";
+	cout << "6 2003р Першого Вересня\n";
+	cout << "7 2004р. Першого Вересня\n";
+	cout << "8 Скрипка\n";
+	cout << "9 Піаніно\n";
+	cout << "10 2012р. закінчення навчання в муз школі.\n";
+	cout << "11 9 клас \n";
+	cout << "12 2014 рік\n";
+	cout << "13 Одеский коледж комп'ютерних техгологій 2014р\n";
+	cout << "14 Захистив диплом на 5 балів\n";
+	cout << "15 2017р\n";
+	cout << "16 Курси булгалтерсього обліку\n";
+	cout << "17 Донну 2017\n";
+	cout << "18 Музика\n";
+	cout << "19 3-д моделювання\n";
+	cout << "20 Спорт\n";
+	cout << "__________МУЗИКА\n";
+	//PlaySound("Sound.wav", NULL, SND_ASYNC);
+	//PlaySound("Sound.wav", NULL, SND_FILENAME | SND_ASYNC );
+	PlaySound(MAKEINTRESOURCE(IDR_WAVE1), NULL, SND_RESOURCE | SND_ASYNC);
+	cout << "__________МУЗИКА\n";
 	system("pause");
-    return 0;
+	return 0;
 }
 
